0928480a5f028ba8d23250682f1d89574d811ba2
mohammed shajin <mohammedshajin1997@gmail.com>
2021-06-10 10:20:07 +0530

    changed registeration design

--- core/views.py
+++ core/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.forms.forms import Form
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
+from django.contrib.auth import login 
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'index.html', {'form': form})
+
--- main/urls.py
+++ main/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('account/', include('account.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
