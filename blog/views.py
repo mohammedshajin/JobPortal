55be845a6f5b4eff0daaa577614a2fa8c50131c2
mohammed shajin <mohammedshajin1997@gmail.com>
2021-06-27 10:36:16 +0530

    blog fixed

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 def blog(request):
     all_posts = Post.objects.all()
+    paginator = Paginator(all_posts, 6)
+    page = request.GET.get('page')
+    all_posts = paginator.get_page(page)
+    
     return render(request, 'blog.html', {'all_posts':all_posts})
 
 def post_single(request, post):
